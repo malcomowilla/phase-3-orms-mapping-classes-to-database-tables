ef7c5775d6b176bb00cb9f7972b09842f49a667f
malcomowilla <owillamalcom@gmail.com>
2023-06-16 17:45:16 +0300

    solved

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,73 @@
 require 'bundler'
 Bundler.require
 
-require_relative '../lib/song'
+require_relative "../lib/song"
 
-DB = { conn: SQLite3::Database.new("db/music.db") }
+DB = {conn: SQLite3::Database.new("db/music.db") }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- lib/song.rb
+++ lib/song.rb
@@ -1,48 +1,109 @@
-class Song
 
+
+
+class Song
   attr_accessor :name, :album, :id
 
-  def initialize(name:, album:, id:nil)
+  def initialize(name:, album:, id: nil)
+    @id = id
     @name = name
     @album = album
-    @id = id
   end
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      album TEXT
-    )
+CREATE TABLE IF NOT EXISTS songs(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  album TEXT
+)
     SQL
+
     DB[:conn].execute(sql)
 
-  end
-  # ... rest of song methods
+end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO songs (name, album)
+    def save
+      sql = <<-SQL
+      INSERT INTO songs(name, album)
       VALUES(?, ?)
-
-    SQL
-    # insert the song
-
-
-    DB[:conn].execute(sql, self.name, self.album)
-
-    #get the song ID from the database and save it to the Ruby instance
+    
+      SQL
+      DB[:conn].execute(sql, self.name, self.album)
     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs") [0][0]
-
-    #return the Ruby instance
     self
   end
 
   def self.create(name:, album:)
     song = Song.new(name:name, album:album)
     song.save
-  
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Song
+
+#   attr_accessor :name, :album, :id
+
+#   def initialize(name:, album:, id:nil)
+#     @name = name
+#     @album = album
+#     @id = id
+#   end
+
+#   def self.create_table
+#     sql = <<-SQL
+#     CREATE TABLE IF NOT EXISTS songs (
+#       id INTEGER PRIMARY KEY,
+#       name TEXT,
+#       album TEXT
+#     )
+#     SQL
+#     DB[:conn].execute(sql)
+
+#   end
+#   # ... rest of song methods
+
+#   def save
+#     sql = <<-SQL
+#       INSERT INTO songs (name, album)
+#       VALUES(?, ?)
+
+#     SQL
+#     # insert the song
+
+
+#     DB[:conn].execute(sql, self.name, self.album)
+
+#     #get the song ID from the database and save it to the Ruby instance
+#     self.id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs") [0][0]
+
+#     #return the Ruby instance
+#     self
+#   end
+
+#   def self.create(name:, album:)
+#     song = Song.new(name:name, album:album)
+#     song.save
+  
+#   end
+
+# end
